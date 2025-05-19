0a47995138b25772ddc7154713b986206b3bdba4
Danny Eskandar <danielesk6@gmail.com>
2025-05-19 00:42:58 -0400

    fixed up a lot

--- ui.py
+++ ui.py
@@ -1,30 +1,25 @@
 import pygame
 import sys
 from snake_logic import start_game
+from shared_constants import *
 
 pygame.init()
-
-width, height = 720, 720
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("SpotiSnake - Start Menu")
 clock = pygame.time.Clock()
 
-WHITE = (255, 255, 255)
-LIGHT_BLUE = (173, 216, 230)
-DARK_BLUE = (0, 0, 139)
-BLACK = (0, 0, 0)
 
 font = pygame.font.SysFont("Corbel", 36)
 
 
-def draw_button(text, x, y, w, h, inactive_color, active_color, action=None):
+def draw_button(text, x, y, w, h, inactive_color, active_color, action=None, action_arg = None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
     if x < mouse[0] < x + w and y < mouse[1] < y + h:
         pygame.draw.rect(screen, active_color, (x, y, w, h))
         if click[0] == 1 and action:
-            action()
+            action(action_arg)    
     else:
         pygame.draw.rect(screen, inactive_color, (x, y, w, h))
 
@@ -33,7 +28,7 @@ def draw_button(text, x, y, w, h, inactive_color, active_color, action=None):
     screen.blit(text_surf, text_rect)
 
 
-def quit_game():
+def quit_game(n):
     pygame.quit()
     sys.exit()
 
@@ -46,15 +41,13 @@ def start_menu():
         title_text = font.render("Welcome to SpotiSnake", True, DARK_BLUE)
         title_rect = title_text.get_rect(center=(width // 2, 100))
         screen.blit(title_text, title_rect)
-
-        draw_button("Play", 260, 200, 200, 50, LIGHT_BLUE, DARK_BLUE, lambda: start_game(start_menu))
-        draw_button("Quit", 260, 280, 200, 50, LIGHT_BLUE, DARK_BLUE, quit_game)
+        draw_button("Play", 260, 200, 200, 50, LIGHT_BLUE, DARK_BLUE, start_game,start_menu)
+        draw_button("Quit", 260, 280, 200, 50, LIGHT_BLUE, DARK_BLUE, quit_game, 0)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+
         pygame.display.update()
         clock.tick(60)
-
-    quit_game()
