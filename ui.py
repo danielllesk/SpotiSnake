1f301164b33b1cba79ac8b3af7f3c3d197a7483e
Danny Eskandar <danielesk6@gmail.com>
2025-05-20 03:42:35 -0400

    fixed up a some stuff started album art logic

--- ui.py
+++ ui.py
@@ -7,19 +7,16 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("SpotiSnake - Start Menu")
 clock = pygame.time.Clock()
-
-
 font = pygame.font.SysFont("Corbel", 36)
 
-
-def draw_button(text, x, y, w, h, inactive_color, active_color, action=None, action_arg = None):
+def draw_button(text, x, y, w, h, inactive_color, active_color, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
     if x < mouse[0] < x + w and y < mouse[1] < y + h:
         pygame.draw.rect(screen, active_color, (x, y, w, h))
         if click[0] == 1 and action:
-            action(action_arg)    
+            action()
     else:
         pygame.draw.rect(screen, inactive_color, (x, y, w, h))
 
@@ -27,27 +24,35 @@ def draw_button(text, x, y, w, h, inactive_color, active_color, action=None, act
     text_rect = text_surf.get_rect(center=(x + w // 2, y + h // 2))
     screen.blit(text_surf, text_rect)
 
-
-def quit_game(n):
+def quit_game():
     pygame.quit()
     sys.exit()
 
-
 def start_menu():
     running = True
+    play_clicked = False
+    screen = pygame.display.set_mode((width, height))
     while running:
         screen.fill(WHITE)
 
         title_text = font.render("Welcome to SpotiSnake", True, DARK_BLUE)
         title_rect = title_text.get_rect(center=(width // 2, 100))
         screen.blit(title_text, title_rect)
-        draw_button("Play", 260, 200, 200, 50, LIGHT_BLUE, DARK_BLUE, start_game,start_menu)
-        draw_button("Quit", 260, 280, 200, 50, LIGHT_BLUE, DARK_BLUE, quit_game, 0)
+        draw_button("Play", 260, 200, 200, 50, LIGHT_BLUE, DARK_BLUE, lambda _: None)
+        draw_button("Quit", 260, 280, 200, 50, LIGHT_BLUE, DARK_BLUE, quit_game)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse = pygame.mouse.get_pos()
+                if 260 < mouse[0] < 460 and 200 < mouse[1] < 250:
+                    play_clicked = True
+                    running = False
 
         pygame.display.update()
         clock.tick(60)
+
+    if play_clicked:
+        from snake_logic import start_game
+        start_game()
